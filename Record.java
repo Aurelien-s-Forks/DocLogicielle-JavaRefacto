40c568d146d3b44ba3f4866d8e9c5da61b9e0708
Aurélien Riche <contact@aurelienriche.com>
2020-10-14 14:43:47 +0200

    Refactor Record

--- Record.java
+++ Record.java
@@ -1,38 +1,4 @@
-/** ***************************************************
- * File:  Record.java
- *
- *
- * Copyright (c) 2002-2003 Advanced Applications Total Applications Works.
- * (AATAW)  All Rights Reserved.
- *
- * AATAW grants you ("Licensee") a non-exclusive, royalty free, license to use,
- * modify and redistribute this software in source and binary code form,
- * provided that i) this copyright notice and license appear on all copies of
- * the software; and ii) Licensee does not utilize the software in a manner
- * which is disparaging to AATAW.
- *
- * This software is provided "AS IS," without a warranty of any kind. ALL
- * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
- * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
- * NON-INFRINGEMENT, ARE HEREBY EXCLUDED. AATAW AND ITS LICENSORS SHALL NOT BE
- * LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
- * OR DISTRIBUTING THE SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL AATAW OR ITS
- * LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT,
- * INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF
- * OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGES.
- *
- * This software is not designed or intended for use in on-line control of
- * aircraft, air traffic, aircraft navigation or aircraft communications; or in
- * the design, construction, operation or maintenance of any nuclear
- * facility. Licensee represents and warrants that it will not use or
- * redistribute the Software for such purposes.
- ***************************************************   */
-
-import javax.swing.JOptionPane;
 import java.io.*;
-import javax.swing.*;
 import java.util.*;
 
    /** *******************************************************************
@@ -75,16 +41,11 @@ public class Record  {
       int ii = 0 , loopCtl = 585 , len = 0 ;
       long remm = fileLen - filePos ;
 
-
       sysPrint( "ReadRec() 1a: Remaining bytes is " + remm ) ;
-
       sysPrint( "ReadRec() 1b: Reading ints" ) ;
-
       recID    = file.readInt();
       sysPrint( "ReadRec() 1c: recID  is " +  recID) ;
-
       quantity = file.readInt();
-
       sysPrint( "ReadRec() 2: Reading string" ) ;
 
       /** Read characters until we get to ;;; which
@@ -99,43 +60,34 @@ public class Record  {
             if ( str2.equals( ";;;" ) )
                 break ;
          }
-
          ii++ ;
       }
 
       sysPrint( "ReadRec() 3a: str is " + str ) ;
-
       sysPrint( "ReadRec() 3b: Reading string. ii =s " + ii ) ;
-
       sysPrint(  "ReadRec() 4: The value of str is " + str ) ;
-
       tokens = new StringTokenizer( str , ";;" ) ;
-
 
       recordTokens = new String[ 7 ] ;
 
-
       if ( tokens.countTokens() >= 4 )   {
-         sysPrint(  "ReadRec() 5: The number of tokens is " +
-                   tokens.countTokens() ) ;
+         sysPrint( "ReadRec() 5: The number of tokens is " + tokens.countTokens() ) ;
          ii = 2 ;
 
-         /** Load the tokens into a string array. */
+         // Load the tokens into a string array
          while( ii < 7  )  {
             recordTokens[ ii ] = tokens.nextToken().toString() ;
             ii++ ;
          }
-
 
          toolType  = new String( recordTokens[ 2 ] ) ;
          brandName = new String( recordTokens[ 3 ] ) ;
          partNum   = new String( recordTokens[ 4 ] ) ;
          cost      = new String( recordTokens[ 5 ] ) ;
          toolDesc  = new String( recordTokens[ 6 ] ) ;
-      }
-      else  {
+      } else  {
          sysPrint(  "ReadRec() 6: There are no records to read." );
-      }      //  end of if
+      }
    }
 
    /** ********************************************************
@@ -143,20 +95,18 @@ public class Record  {
     *  blanks.
     ******************************************************** */
     public StringBuffer fill ( String str, StringBuffer buf , int len) {
-       String strTwo = new String( "                     "  +
-           "                                             " ) ;
+       String strTwo = new String( "                     "  + "                                             " ) ;
 
        if ( str != null ) {
           buf.setLength( len );
           buf = new StringBuffer( str + strTwo );
-       }
-       else
+       } else {
           buf = new StringBuffer( strTwo );
+       }
 
        if ( len == 0 ) {
           buf.setLength( 45 );
-       }
-       else {
+       } else {
           buf.setLength( len );
        }
 
@@ -175,74 +125,38 @@ public class Record  {
       String str = "" , str2 = ""  ;
 
       file.writeInt( recID );
-
       file.writeInt( quantity );
 
       str = str + toolType + ";;" ;
-
       str = str + brandName + ";;"  ;
       str = str + partNum + ";;"  ;
       str = str + cost  + ";;" ;
-
       str = str + toolDesc + ";;;"  ;
-
 
       buf = fill ( str , buf.delete(0, 451), 451 ) ;
 
       file.writeChars( buf.toString() );
       sysPrint(  "write(): - The value of recID is " + recID );
       sysPrint(  "write(): - The value of quantity is " + quantity );
-      sysPrint(  "write(): - The value of str2 is " + str + " with a length of "
-                   + str2.length() );
+      sysPrint(  "write(): - The value of str2 is " + str + " with a length of " + str2.length() );
       sysPrint(  "write(): - The length of buf is " + buf.length() );
-
    }
 
-
-   /** ********************************************************
-    * Method: writeInteger() is used to wite an integer to the
-    *         randomaccess file.
-    ********************************************************/
    public void writeInteger( RandomAccessFile file , int a ) throws IOException
    {
-
       file.writeInt( a );
    }
 
-
-   /** ********************************************************
-    * Method: getRecID() is used to obtain the record ID.
-    ********************************************************/
    public int getRecID() { return recID; }
 
-
-   /** ********************************************************
-    * Method: getToolType() is used to obtain the tool type.
-    ********************************************************/
    public String getToolType() { return toolType.trim(); }
 
-   /** ********************************************************
-    * Method: getToolDesc() is used to obtain the description of
-    *         the tool.
-    ********************************************************/
    public String getToolDesc() { return toolDesc.trim(); }
 
-   /** ********************************************************
-    * Method: getPartNumber() is used to obtain the Part Number
-    *         value currently in the record.
-    ********************************************************/
    public String getPartNumber() { return partNum.trim(); }
 
-   /** ********************************************************
-    * Method: getQuantity() is used to obtain the value of the
-    *         quantity currently in the record.
-    ********************************************************/
    public int getQuantity() { return quantity; }
 
-   /** ********************************************************
-    * Method: getBrandName() is used to obtain the value of the
-    *         current value of Brand Name from the record.
-    ********************************************************/
    public String getBrandName() { return brandName.trim(); }
 
    /** ********************************************************
