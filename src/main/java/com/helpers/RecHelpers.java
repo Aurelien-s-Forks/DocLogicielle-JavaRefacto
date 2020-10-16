b785d3d737b011748849fd2b9fc6f9b4307a9c39
Fi Skirata <43534557+Lvgjann@users.noreply.github.com>
2020-10-16 15:32:13 +0200

    Corrected merge error.

--- src/main/java/com/helpers/RecHelpers.java
+++ src/main/java/com/helpers/RecHelpers.java
@@ -8,11 +8,7 @@ import java.util.Arrays;
 
 public class RecHelpers extends Container {
 
-<<<<<<< HEAD
     private final ArrayList<String> allLabels = new ArrayList<>(Arrays.asList("Record ID", "Type of Tool", "Brand Name", "Tool Description", "Part Number", "Quantity", "Price"));
-=======
-    private ArrayList<String> allLabels = new ArrayList<String>(Arrays.asList("Record ID", "Type of Tool", "Brand Name", "Tool Description", "Part Number", "Quantity", "Price"));
->>>>>>> 565eee926a5ea6335708958569ef1cee68e0e160
 
     public static void addLabel(String label) {
         JLabel newLabel = new JLabel(label);
@@ -20,13 +16,7 @@ public class RecHelpers extends Container {
     }
 
     public void addAllLabels() {
-<<<<<<< HEAD
         for (String allLabel : allLabels) RecHelpers.addLabel(allLabel);
-=======
-        for (String allLabel : allLabels) {
-            RecHelpers.addLabel(allLabel);
-        }
->>>>>>> 565eee926a5ea6335708958569ef1cee68e0e160
     }
 
     public static void createButtonsActionListener(JButton save, JButton cancel, ActionListener newRec) {
@@ -37,18 +27,10 @@ public class RecHelpers extends Container {
     }
 
     public static void addAllTextField(ArrayList<JTextField> jTextFieldsList) {
-<<<<<<< HEAD
         for (JTextField jTextField : jTextFieldsList) new Container().add(jTextField);
     }
 
     public ArrayList<String> getAllLabels() {
         return allLabels;
     }
-=======
-        for (JTextField jTextField : jTextFieldsList) {
-            new Container().add(jTextField);
-        }
-    }
-
->>>>>>> 565eee926a5ea6335708958569ef1cee68e0e160
 }
